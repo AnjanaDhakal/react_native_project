f00b64ea06af974e5741a4febd09d45396f92e08
Anjana Dhakal <80588450+AnjanaDhakal@users.noreply.github.com>
2026-01-05 12:26:24 +0545

    Simple React Native Project with Tailwind CSS

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,33 +1,14 @@
-import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-import { StoreProvider } from '@/context/StoreContext';
-import { AuthProvider } from '@/context/AuthContext';
-import { TodoProvider } from '@/context/TodoContext';
-import { NotificationProvider } from '@/context/NotificationContext';
 import '../global.css';
 
 export default function RootLayout() {
-  useFrameworkReady();
-
   return (
-    <StoreProvider>
-      <AuthProvider>
-        <NotificationProvider>
-          <TodoProvider>
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="login" />
-              <Stack.Screen name="register" />
-              <Stack.Screen name="(tabs)" />
-              <Stack.Screen name="+not-found" />
-              <Stack.Screen name="(main)/dashboard" />
-              <Stack.Screen name="(main)/scanner" />
-            </Stack>
-            <StatusBar style="auto" />
-          </TodoProvider>
-        </NotificationProvider>
-      </AuthProvider>
-    </StoreProvider>
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
   );
 }
